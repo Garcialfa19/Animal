475e6a7f85c934438b516b0afb9ad85ebe30c39d
David Garcia <garcialfa19@icloud.com>
2021-02-03 23:20:53 -0600

    First Commit

---
+++ src/David/ClassWork/Animal.java
@@ -0,0 +1,69 @@
+package David.ClassWork;
+
+public class Animal {
+    String name;
+    String color;
+    String species;
+    String gender;
+    int age;
+    String height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Name=" + name + '\'' +
+                ", Color=" + color + '\'' +
+                ", Species=" + species + '\'' +
+                ", Gender=" + gender + '\'' +
+                ", Age=" + age +
+                ", Height=" + height;
+    }
+}
+
---
+++ src/David/ClassWork/Main.java
@@ -0,0 +1,20 @@
+package David.ClassWork;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal Dog = new Animal();
+
+
+        Dog.name = "Jack";
+        Dog.color = "Black";
+        Dog.species = "Rottweiler";
+        Dog.gender = "Male";
+        Dog.age = 11;
+        Dog.height = "55cm";
+
+        System.out.println(Dog);
+
+    }
+}
